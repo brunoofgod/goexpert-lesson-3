3adcd2e9216bcf51d26ecd4472f7c544d009e701
Bruno de Deus <bruno.deus@linedata.com.br>
2025-01-23 17:40:06 -0300

    Fixes

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -48,7 +48,7 @@ func main() {
 	webOrderHandler := NewWebOrderHandler(db, eventDispatcher)
 
 	webserver.AddHandler("POST", "/order", webOrderHandler.Create)
-	webserver.AddHandler("GET", "/orders", webOrderHandler.List)
+	webserver.AddHandler("GET", "/order", webOrderHandler.List)
 	fmt.Println("Starting web server on port", configs.WebServerPort)
 	go webserver.Start()
 
